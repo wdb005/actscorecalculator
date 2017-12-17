76d08d8b8c747c83b7808b3a5aaaad6d5bc5d2db
wdb005 <33012557+wdb005@users.noreply.github.com>
2017-12-16 22:17:46 -0500

    Version 2.0

---
+++ actcompcalc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#Use this program to calculate your ACT composite score
+#Version 2.0
+mathScore = int(input('Type in your math score '))
+englishScore = int(input('Type in your english score '))
+readingScore = int(input('Type your reading score '))
+scienceScore = int(input('Type your science score '))
+
+#This calculates the composite score
+compScore = float(mathScore + readingScore + englishScore + scienceScore)/float(4)
+print(float(compScore))
+
+#This section of code offers the users feedback regarding their score
+if compScore > 33:
+    print('Wow, Enjoy MIT')
+if compScore < 33 and compScore > 29:
+    print('Smart kiddo aye')
+if compScore > 24 and compScore < 29:
+    print("Good work, you can probably get into a solid state school.")
+if compScore < 24:
+    print("Study and retake, you can do better")
+    
